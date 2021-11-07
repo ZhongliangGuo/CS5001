e37336cb11de2e1b62826b0da0365d9387396c56
ZhongliangGuo <93882634+ZhongliangGuo@users.noreply.github.com>
2021-11-07 19:34:21 +0000

    Add files via upload

---
+++ CS5001-p0-hello/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		if(args.length==0){
+			System.out.println("Hello World");
+		}
+		else{
+			System.out.println("Hello "+args[0]);
+		}
+	}
+}
